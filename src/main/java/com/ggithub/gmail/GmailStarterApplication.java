e8a5c7aed77f606982e5643805f205b6d9854518
hellcrow <bakamakafoqq@gmail.com>
2022-01-07 18:04:26 +0200

    first commit

---
+++ src/main/java/com/ggithub/gmail/GmailStarterApplication.java
@@ -0,0 +1,13 @@
+package com.ggithub.gmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmailStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ggithub/gmail/GmailStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggithub.gmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
